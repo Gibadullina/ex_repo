5306dda5c6c4909d770d770c36d6405535bd017d
Gibadullina <gibadullina.elina@inbox.ru>
2024-04-20 21:28:16 +0500

    #14 выполнено задание 6 (способ 1) для 3 урока

---
+++ lesson_3/ex_6.py
@@ -0,0 +1,22 @@
+# Задание 6
+
+# 1 способ
+print('Способ 1 цикл for')
+
+number1 = int(input('Введите число: '))
+count1 = 0
+
+for x in range(number1):
+    count1 += 1
+print(f'Количество цифр в числе = {count1}')
+
+# 2 способ
+print('\nСпособ 2 цикл while')
+
+number2 = int(input('Введите число: '))
+count2 = 0
+
+while number2 > 0:
+    count2 += 1
+    number2 //= 10
+print(f'Количество цифр в числе = {count2}')
